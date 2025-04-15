b23d4bb69227d7e691142f1ca3392b2c3fb66a7f
Jususs <j.warm@gmx.at>
2025-04-15 12:54:10 +0200

    inital project commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+//TODO: img wont show up even tho embedded as src
+
+export const Footer = () => {
+    return (
+        <div className="font-bold from-black to-white">
+            <div className="flex justify-center text-center">
+                <img
+                    src="../img/support_bar_township_moedling.png"
+                    alt="support banner township moedling"
+                    className="h-[60px] w-[424px] mt-1"
+                ></img>
+            </div>
+        </div>
+    );
+};
